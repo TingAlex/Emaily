34c1e498cf8853eea8f68bc2db4b52d48b387a10
Ting <tingalex30@gmail.com>
2018-03-24 09:00:48 +0800

    add github auth

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-module.exports={
-    googleClientID:"294766320364-v7k8ck0re3f7g5qlfvv7e366uitsipn1.apps.googleusercontent.com",
-    googleClientSecret:"DkPp-W_dmmBL4JuMrQyBZIv4"
-}
+module.exports = {
+  googleClientID:
+    "294766320364-v7k8ck0re3f7g5qlfvv7e366uitsipn1.apps.googleusercontent.com",
+  googleClientSecret: "DkPp-W_dmmBL4JuMrQyBZIv4",
+  githubClientID: "e6695a60dbcdb1bf75cf",
+  githubClientSecret: "4a0fa2c57b14b8cdbf30a979291bcbfc2ff47dd3"
+};
