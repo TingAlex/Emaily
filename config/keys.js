24be43ea837bb99fc35c80e4bc99fffba74cd9a9
Ting <tingalex30@gmail.com>
2018-03-27 16:00:51 +0800

    add logout part and cookie

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "294766320364-v7k8ck0re3f7g5qlfvv7e366uitsipn1.apps.googleusercontent.com",
+  googleClientSecret: "DkPp-W_dmmBL4JuMrQyBZIv4",
+  githubClientID: "e6695a60dbcdb1bf75cf",
+  githubClientSecret: "4a0fa2c57b14b8cdbf30a979291bcbfc2ff47dd3",
+  mongoURI: "mongodb://ting:chongzheng10@ds223509.mlab.com:23509/emaily-devlop",
+  cookieKey:'oingaerihvdfiduvshagn3534treght3'
+};
