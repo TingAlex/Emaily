6acbbbc06ad732f226f5e2b814c3c10cde7a3cd2
Ting <tingalex30@gmail.com>
2018-03-27 08:32:29 +0800

    Add cookie depatch part

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String
+  githubId: String
 });
 //A new collection named users
 mongoose.model("users", userSchema);
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const mongoose = require("mongoose");
 const keys = require("../config/keys");
 //User is our model class
 const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  //mongoo generate the user.id automally, use this rather googleId is because people could log in with a different OAuth.
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -29,10 +40,22 @@ passport.use(
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-      // console.log("accessToken", accessToken);
+      console.log("accessToken", accessToken);
       // console.log("refreshToken", refreshToken);
       // console.log("profile", profile);
-      new User({ googleID: profile.id });
+      // console.log("githubId:", profile.id);
+      User.findOne({ githubId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //aleady have a record before
+          done(null, existingUser);
+        } else {
+          //make new record
+          //need save() to
+          new User({ githubId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
