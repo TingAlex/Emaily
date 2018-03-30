46af8a158e5c4069382e391e03f7a2d4992b4435
Ting <tingalex30@gmail.com>
2018-03-30 10:53:44 +0800

    add google auth to debug

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,8 @@
 //prod.js - production keys here!
 module.exports = {
+  googleClientID:
+    "294766320364-v7k8ck0re3f7g5qlfvv7e366uitsipn1.apps.googleusercontent.com",
+  googleClientSecret: "DkPp-W_dmmBL4JuMrQyBZIv4",
   githubClientID: process.env.GITHUB_CLIENT_ID,
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
