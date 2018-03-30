00e1740dc9d9228ed9ed2337239874e0784aee40
Ting <tingalex30@gmail.com>
2018-03-30 10:37:16 +0800

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//prod.js - production keys here!
+module.exports = {
+  githubClientID: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
